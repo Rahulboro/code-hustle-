47d84c25e6099b0413d1149c9f3266e70b3bb7f1
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2024-06-04 22:55:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 2,
+  speed: 600,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 3,
+    slideShadows: true,
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: false,
+  },
+});
+
+function toggleMenu() {
+  const navLinks = document.getElementsByClassName("nav-right-section");
+  navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
+}
